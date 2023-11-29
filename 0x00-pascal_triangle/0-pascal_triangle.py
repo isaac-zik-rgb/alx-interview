2ba352b52515a10162d0168a71eae37d2dd61ce8
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-29 15:39:37 +0100

    Add pascal_triangle function to generate Pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Pascal's Triangle"""
 
 def pascal_triangle(n):
+    '''Returns a list of lists of integers representing
+the Pascal’s triangle of n'''
     arr = []
     if n <= 0:
         
