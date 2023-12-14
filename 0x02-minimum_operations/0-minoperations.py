c45412291e28e3c4a421a0cb652cbe47432daf09
Isaac Okechukwu Chukwuma <isaacokechukwu200021@gmail.com>
2023-12-14 21:31:58 +0100

    0. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Minimum Operations """
+
+
+def minOperations(n):
+    """ Method that calculates the fewest number of operations
+        needed to result in exactly n H characters in the file """
+
+    operation = 0
+    div = 2
+
+    if n <= 1:
+        return 0
+    while n > 1:
+        if n % div == 0:
+            operation += div
+            n /= div
+        else:
+            div += 1
+    return operation
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,7 @@
+# 0x02-minimum_operations
+# File: README.md
+# Created on: 2020-01-25 16:00:00
+# Created by: Isaac Castro
+# -*- coding: utf-8 -*-
+"""Minimum Operations
+"""
