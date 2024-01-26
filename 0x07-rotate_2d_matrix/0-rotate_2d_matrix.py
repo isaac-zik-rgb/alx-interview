8f7a237e6137169ed0bd5fafbea6efc3668252d2
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-26 17:13:03 +0100

    Update function to handle edge case
    This commit updates the function to handle an edge case that was causing an error. The changes include adding a conditional statement to check for the edge case and implementing the necessary logic to handle it properly.

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+
+"""0. Rotate 2D Matrix"""
+
+def rotate_2d_matrix(matrix):
+
+    """Given an n x n 2D matrix, rotate
+    it 90 degrees clockwise.
+    """
+    reversed_matrix = matrix[::-1]
+    columns = len(reversed_matrix)
+    rows = len(reversed_matrix[0])
+    myArry = [[0] * columns for _ in range(rows)]
+    for i in range(columns):
+        for j in range(rows):
+            myArry[j][i] = reversed_matrix[i][j]
+    
+    return myArry
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,2 @@
+# 0x07-rotate_2d_matrix
+this project is a interview project rotate a 2D matrix
