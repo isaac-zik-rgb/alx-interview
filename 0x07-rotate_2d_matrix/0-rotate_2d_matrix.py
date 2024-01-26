4c9df514a5c26893d75b6c45aab1750b5fa295fc
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-26 17:38:06 +0100

    fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""0. Rotate 2D Matrix"""
+"""Rotate 2D Matrix"""
+
 
 def rotate_2d_matrix(matrix):
-
-    """Given an n x n 2D matrix, rotate
-    it 90 degrees clockwise.
     """
+    Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+    """
+    # Reverse the matrix
     reversed_matrix = matrix[::-1]
-    columns = len(reversed_matrix)
-    rows = len(reversed_matrix[0])
-    myArry = [[0] * columns for _ in range(rows)]
-    for i in range(columns):
-        for j in range(rows):
-            myArry[j][i] = reversed_matrix[i][j]
-    
-    return myArry
+
+    # Get the number of columns and rows
+    columns, rows = len(reversed_matrix), len(reversed_matrix[0])
+
+    # Create a new matrix with rotated values
+    rotated_matrix = [[reversed_matrix[i][j] for i in range(columns)] for j in range(rows)]
+
+    return rotated_matrix
+
